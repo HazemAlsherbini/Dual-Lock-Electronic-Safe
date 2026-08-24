c97e1b0ae755fe490a22b70e6f9b1edc77bcbcfa
Mohamed Saeed <mtealab416@gmail.com>
2026-08-24 19:27:28 +0300

    Receive password from the user

--- SOURCE/APP/main.c
+++ SOURCE/APP/main.c
@@ -1,23 +1,59 @@
-/*
- * main.c
- *
- *  Created on: Aug 18, 2026
- *      Author: zom22
- */
-// Library Directives
+
+
 #include "../../INCLUDE/LIB/BIT_MATH.h"
 #include "../../INCLUDE/LIB/STD_TYPES.h"
 
-// DIO Directives
 #include "../../INCLUDE/MCAL/DIO/DIO_INTERFACE.h"
 #include "../../INCLUDE/MCAL/DIO/DIO_PRIVATE.h"
 #include "../../INCLUDE/MCAL/DIO/DIO_CFG.h"
-//#include <stdio.h>
+
+#include "../../INCLUDE/HAL/LCD/LCD_INTERFACE.h"
+
+
+#include "../../INCLUDE/HAL/KEYPAD/KEYPAD_INTERFACE.h"
+
 #include <util/delay.h>
+
+
 
 int main(void)
 {
+    MDIO_voidInit();     /* initialize dio before asking lcd/keypad to work */
+    HLCD_voidInit();     /* initialize lcd */
+
+    u8 L_u8EnteredPassword[4];
+    u8 L_u8PressedKey = 0xFF;
+    u8 L_u8EnteredNumbersCounter = 0;
+
+    HLCD_voidSendString((u8 *)"Enter Password:");
+    HLCD_voidGoToPos(ROW2, col1);
+    while (1)
+    {
+        L_u8PressedKey = HKEYPAD_u8GetPressedKey();
+
+        if ((L_u8PressedKey >= '0') && (L_u8PressedKey <= '9'))
+
+        {
+            L_u8EnteredPassword[L_u8EnteredNumbersCounter] = L_u8PressedKey;
 
 
+            HLCD_voidSendData('*');
+
+
+            L_u8EnteredNumbersCounter++;
+
+            if (L_u8EnteredNumbersCounter == 4)
+
+            {
+                _delay_ms(500);
+                HLCD_voidClearDisplay();
+                HLCD_voidSendString((u8 *)"Password Stored!");
+
+
+
+            }
+        }
+    }
+
+    return 0;
 }
-
